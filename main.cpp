bafdaf3d65dfb006db44f15a832b71d67cb9bfa7
ShihaoCai <a1799366@student.adelaide.edu.au>
2022-08-08 07:18:25 +0930

    prac1

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "Human.cpp"
+#include "Computer.cpp"
+#include "Referee.cpp "
+
+using namespace std;
+
+int main()
+{
+   Human H;
+   Computer C;
+   Referee R;
+
+   R.competition( H, C );
+
+   return 0;
+}
